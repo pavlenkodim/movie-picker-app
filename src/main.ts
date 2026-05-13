b3b0060e4e103ac814993084b240399be32c3397
Dmitry Pavlenko <pavlenkodim@mail.ru>
2026-05-13 18:00:25 +0500

    Fix: remove export bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
-export async function bootstrap() {
+async function bootstrap() {
   const PORT = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule);
